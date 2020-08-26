906c7b1ef79e7d4ca5623fbbaabbbfae65dfa1a4
github username <31523060+elishevaelbaz@users.noreply.github.com>
2020-08-26 13:27:01 -0400

    cors url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001', 'https://typ3out.netlify.app/' #when deploy, change to also include hosted frontend server
+    origins 'localhost:3001', 'https://typ3out.netlify.app' #when deploy, change to also include hosted frontend server
 
     resource '*',
       headers: :any,
